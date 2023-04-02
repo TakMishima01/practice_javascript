4e5619a5a53e289233feddc6c6c4f58f115c89b6
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-04-03 07:02:46 +0900

    chapter finish

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-let hello = "hello world";
-alert(hello);
+// let hello = "hello world";
+// alert(hello);
+
+// alert(4+3);
+
+// alert('Hello'+'world');
+
+// let str1 = 'Hello';
+// let str2 = 'World';
+// alert(str1 + str2);
+
+// let orange = 120;
+// let apple = 120;
+
+// if (orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+

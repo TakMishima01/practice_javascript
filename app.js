be62feee9d5c1e2edd029858b08cb54bb1941abf
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-04-05 06:41:22 +0900

    変数宣言学習

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// ----------基本文法学習----------
+
 // let hello = "hello world";
 // alert(hello);
 
@@ -39,3 +41,75 @@
 // }
 
 // alert('１から１０まで足した結果は'+ num + 'です');
+
+// ----------変数宣言学習----------
+
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// var str = "webcamp"
+
+// function foo(){
+//     console.log(str)
+//     var y = "hello"
+// }
+
+// foo()
+// console.log(y)
+
+// function foo(){
+//     let x = "webcamp"
+//     {
+//         let y = "hello webcamp"
+//     }
+//     console.log(x)
+//     console.log(Y)
+// }
+
+// foo()
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+// console.log(i)
+
+// var str = "webcamp"
+
+// function foo(){
+//     console.log(str)
+//     var str = "dmm webcamp"
+//     console.log(str)
+// }
+
+// foo()
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+// }

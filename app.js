3d268a6193f15b34f0dc0f01f69dfedb24f0819c
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-04-06 07:02:58 +0900

    [途中]関数学習

--- app.js
+++ app.js
@@ -128,3 +128,38 @@
 // }
 
 
+// let promptStr = prompt('なにか好きな文字を入力してください。');
+
+// alert(promptStr);
+
+
+// let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+// alert('あなたの選んだ手は' + user_hand + 'です。');
+
+
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+alert('あなたの選んだ手は' + user_hand + 'です。＼nJavaScriptの選んだ手は' + js_hand + 'です。＼n結果は' + judge + 'です。');
+
+function getJShand(){
+    let js_hand_num = Math.floor(Math.random() * 3);
+    let hand_name;
+
+    if(js_hand_num == 0){
+        js_hand = "グー";
+    } else if(js_hand_num == 1){
+        js_hand = "チョキ";
+    } else if(js_hand_num == 2){
+        js_hand = "パー";
+    }
+
+    return js_hand;
+}
+
+function winLose

06bde38d8530286dfd599e0a5c042665953cfc4a
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-04-10 08:08:11 +0900

    [途中]jQuery

--- app.js
+++ app.js
@@ -234,12 +234,44 @@
 //   $('.box1').css({'background-color': '#0000FF'});
 // });
 
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px',
+//     'width': '200px'
+//     }).slideUp();
+//   });
+// });
+
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px',
-    'width': '200px'
-    }).slideUp();
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
